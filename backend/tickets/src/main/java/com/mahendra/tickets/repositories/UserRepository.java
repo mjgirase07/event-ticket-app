5ccd80493fb98c14e534b22d93e7fa4eb819f4bf
Mahendra Girase <2021.mahendra.girase@ves.ac.in>
2026-05-11 11:48:29 +0530

    addfeat(auth): setup Spring Security with provisioning filter and audit config

---
+++ backend/tickets/src/main/java/com/mahendra/tickets/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.mahendra.tickets.repositories;
+
+import com.mahendra.tickets.domain.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
