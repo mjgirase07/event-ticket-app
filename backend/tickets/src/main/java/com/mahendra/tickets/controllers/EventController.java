61f85288169cd6bedd6254753bd822852326a08d
Mahendra Girase <2021.mahendra.girase@ves.ac.in>
2026-05-12 12:04:34 +0530

    feat(event): add EventController with createEvent API

---
+++ backend/tickets/src/main/java/com/mahendra/tickets/controllers/EventController.java
@@ -0,0 +1,43 @@
+package com.mahendra.tickets.controllers;
+
+import com.mahendra.tickets.domain.CreateEventRequest;
+import com.mahendra.tickets.domain.dtos.CreateEventRequestDto;
+import com.mahendra.tickets.domain.dtos.CreateEventResponseDto;
+import com.mahendra.tickets.domain.entities.Event;
+import com.mahendra.tickets.mappers.EventMapper;
+import com.mahendra.tickets.services.EventService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+import static com.mahendra.tickets.util.JwtUtil.parseUserId;
+
+@RestController
+@RequestMapping(path = "/api/v1/events")
+@RequiredArgsConstructor
+public class EventController {
+
+    private final EventMapper eventMapper;
+    private final EventService eventService;
+
+    @PostMapping
+    public ResponseEntity<CreateEventResponseDto> createEvent(
+            @AuthenticationPrincipal Jwt jwt,
+            @Valid @RequestBody CreateEventRequestDto createEventRequestDto) {
+        CreateEventRequest createEventRequest = eventMapper.fromDto(createEventRequestDto);
+        UUID userId = parseUserId(jwt);
+
+        Event createdEvent = eventService.createEvent(userId,createEventRequest);
+        CreateEventResponseDto createEventResponseDto = eventMapper.toDto(createdEvent);
+        return new ResponseEntity<>(createEventResponseDto, HttpStatus.CREATED);
+    }
+}
---
+++ backend/tickets/src/main/java/com/mahendra/tickets/util/JwtUtil.java
@@ -0,0 +1,14 @@
+package com.mahendra.tickets.util;
+
+import org.springframework.security.oauth2.jwt.Jwt;
+
+import java.util.UUID;
+
+public class JwtUtil {
+    private JwtUtil(){
+    }
+
+    public static UUID parseUserId(Jwt jwt){
+        return UUID.fromString(jwt.getSubject());
+    }
+}
